0b15062b4deca09245ac231d2706087f957385f9
Álvaro Aberastain Marzorati <a.aberastain.marzorati@gmail.com>
2021-08-13 11:31:54 -0300

    fixed the for loops and added comments

--- script.js
+++ script.js
@@ -1,48 +1,69 @@
-const selector = document.getElementById('sizes');
-const container = document.getElementById('board');
-const paintColorSel = document.getElementById('paint-color');
-const boardColorSel = document.getElementById('board-color');
-
-const normalText = document.querySelector("#normal-text");
-const rndmText = document.querySelector("#rndm-text");
-const lightenText = document.querySelector("#lighten-text");
-const darkenText = document.querySelector("#darken-text");
-
-let currentBoardColor = '#ffffff';
+//  Sets the size input to a variable and asigns the default value
+const selector = document.getElementById('sizes'); 
 let size = selector.value;
-const resetBtn = document.getElementById('reset');
 
-/*I don't know why if I dont divide by 10 then it creates a grid of size*10xsize*10 
-The size value is correct, I guess the problem is in the loops (CONSOLE.LOGS IN THE LOOPS FOR DEBUG)
-And that only happens when It makes the grid as a function*/
+
+/*  With the size value, creates a grid of divs.
+Each div is assigned to a row and a column and setted to "currentBoardColor". */
+const container = document.getElementById('board');
+let currentBoardColor = '#ffffff';
 function makeGrid() {
-    for (let i = 1; i < (size + 1)/10 ; i++) { 
-        for (let j = 1; j < (size + 1)/10 ; j++) {
+    for (let i = 1; i <= size; i++) {  //One 'for' loop is for the rows (i) and the other one is for the columns (j).
+        for (let j = 1; j <= size ; j++) {
             let d = document.createElement('div');
             d.setAttribute('class', 'square');
             d.style.gridRow = i;
             d.style.gridColumn = j;
-            d.style.opacity = '1';
-            d.style.backgroundColor = currentBoardColor;
-            container.appendChild(d);
+            d.style.backgroundColor = currentBoardColor; //At the start, that current board color is white.
+            container.appendChild(d); //The finished div is contained by the board.
         };
     };
 };
-let allSq;
-let paint = false;
+
+//  Sets another variable with the board but just for changing its border color as an indicator
 const ind = document.querySelector("#board");
 ind.style.borderColor = '#5c5c74';
+
+/*  "updateSwitches" sets the menu style to indicate the current setting. 
+The first parameter is the one activated.
+The "normal" setting is the default */
+const normalText = document.querySelector("#normal-text");
+const rndmText = document.querySelector("#rndm-text");
+const lightenText = document.querySelector("#lighten-text");
+const darkenText = document.querySelector("#darken-text");
+function updateSwitches(swUp,swDown1,swDown2,swDown3) {
+    swUp.style.color = '#f5efc7';
+    swUp.style.textShadow = '0 0 4px #e2d99e';
+    swUp.style.textDecoration = 'underline';
+    swDown1.style.color = '#b6b191';
+    swDown1.style.textShadow = 'none';
+    swDown1.style.textDecoration = 'none';
+    swDown2.style.color = '#b6b191';
+    swDown2.style.textShadow = 'none';
+    swDown2.style.textDecoration = 'none';
+    swDown3.style.color = '#b6b191';
+    swDown3.style.textShadow = 'none';
+    swDown3.style.textDecoration = 'none';
+};
 updateSwitches(normalText,rndmText,lightenText,darkenText);
 
+/*  setGame creates and sets all the elements necessary to play.
+This function is called at the start and then each time the size changes. */
+let allSq;
+let paint = false;
 function setGame() {
     makeGrid();
     allSq = Array.from(document.getElementsByClassName('square'));
-    allSq.forEach(square => square.addEventListener('click', () => {
+    allSq.forEach(square => square.addEventListener('click', () => { // If a div is clicked, the painting mode goes 'on' or 'off'
         paint = !paint;
-        (paint) ? ind.style.borderColor = '#e2d99e' : ind.style.borderColor = '#5c5c74';
+        (paint) ? ind.style.borderColor = '#e2d99e' : ind.style.borderColor = '#5c5c74'; // Indication border
     }));
-    allSq.forEach(square => square.addEventListener('mouseover', coloring));
-}
+    allSq.forEach(square => square.addEventListener('mouseover', coloring)); // Sets each div and event listener to call the "coloring" function
+};
+setGame();
+
+/*  This function gets a rgb string value such as "rgb(255, 255, 255)" and returns that value in hex code like "#ffffff"
+Solves some errors when trying to compare color values in different formats. */
 function rgbToHex (rgb) {
     if (rgb[0] == '#') {
         return rgb;
@@ -97,6 +118,8 @@ function rgbToHex (rgb) {
         }
     }
 };
+
+//  Function that receives a color and adds that color a bit of red, green and blue, unless it is already white (full rgb values).
 function lighten(color) {
     let red, green, blue;
     if (color !== '#000')  {
@@ -111,6 +134,8 @@ function lighten(color) {
     }
     return '#'+red+green+blue;
 };
+
+//  Function that receives a color and removes that color a bit of red, green and blue, unless it is already black (minimum rgb values).
 function darken(color) {
     let red, green, blue;
     if (color !== '#000' && color !== '#000000')  {
@@ -126,17 +151,9 @@ function darken(color) {
     return '#'+red+green+blue;
 };
 
-//Function that takes and rgb value with the format rgb(nnn, nnn, nnn) and returns it in hex code #hhhhhh
-//This is necessary for the color comparisson for the board color change
-
-
-setGame();
-
-let setting = 'normal';
-
-
-//Setting the event listener to the grid
-
+//  According to the current setting, "coloring" paints the div.
+let setting = 'normal'; // By default
+const paintColorSel = document.getElementById('paint-color');
 function coloring() {
     if (paint) {
         switch(setting) {
@@ -155,7 +172,8 @@ function coloring() {
     }
 };
 
-//Switches to change 'setting'
+//  Event listeners for the settings switches / buttons. Each one changes the current setting and updates the styles
+
 normalText.addEventListener('click', () => {
     setting = 'normal';
     lightenSwitch.checked = false;
@@ -200,31 +218,15 @@ darkenSwitch.addEventListener('change', () => {
     }
 });
 
-
-function updateSwitches(swUp,swDown1,swDown2,swDown3) {
-    swUp.style.color = '#f5efc7';
-    swUp.style.textShadow = '0 0 4px #e2d99e';
-    swUp.style.textDecoration = 'underline';
-    swDown1.style.color = '#b6b191';
-    swDown1.style.textShadow = 'none';
-    swDown1.style.textDecoration = 'none';
-    swDown2.style.color = '#b6b191';
-    swDown2.style.textShadow = 'none';
-    swDown2.style.textDecoration = 'none';
-    swDown3.style.color = '#b6b191';
-    swDown3.style.textShadow = 'none';
-    swDown3.style.textDecoration = 'none';
-}
-
-
-//Size changer
+//  Size changer. Deletes the board and sets a new game with the new size.
 selector.addEventListener('change', ()=>{
     size = selector.value;
-    container.innerHTML = ''; //This line sets the cointainer to have no content
+    container.innerHTML = '';
     setGame();
 });
 
-//Reset button to turn the board white
+//  Reset button that turns the board white.
+const resetBtn = document.getElementById('reset');
 resetBtn.addEventListener('click', ()=>{
     for (let k = 0; k < (size*size); k++) {
         allSq[k].style.backgroundColor = '#ffffff';
@@ -232,7 +234,8 @@ resetBtn.addEventListener('click', ()=>{
     currentBoardColor = '#ffffff';
 });
 
- //Board color change
+ // Board color changer.
+ const boardColorSel = document.getElementById('board-color');
  boardColorSel.addEventListener('change', ()=>{
     for (let q = 0; q < (size*size); q++) {
             if (rgbToHex(allSq[q].style.backgroundColor) == currentBoardColor) {
